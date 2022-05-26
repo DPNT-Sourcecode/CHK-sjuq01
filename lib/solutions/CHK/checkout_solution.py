d04293f5ca2e4f5c00d727d2117a81a41cf08021
sbx_user1051 <sbx_user1051@169.254.33.21>
2022-05-26 20:44:22 +0000

    Thu May 26 20:44:22 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,24 +10,45 @@ def checkout(skus):
         "C": 20,
         "D": 15,
         "E": 40,
-        "F": 10
+        "F": 10,
+        "G": 20,
+        "H": 10,
+        "I": 35,
+        "J": 60,
+        "K": 80,
+        "L": 90,
+        "M": 15,
+        "N": 40,
+        "O": 10,
+        "P": 50,
+        "Q": 30,
+        "R": 50,
+        "S": 30,
+        "T": 20,
+        "U": 40,
+        "V": 50,
+        "W": 20,
+        "X": 90,
+        "Y": 10,
+        "Z": 50
     }
     offers = {
         "A": [[5,200],[3,130]],
         "B": [[2,45]],
         "E": [[2,"B"]],
-        "F": [[3,20]]
+        "F": [[3,20]],
+        "H": [[10,80],[5,45]],
+        "K": [[2,150]],
+        "N": [[3,"M"]],
+        "P": [[5,200]],
+        "Q": [[3,80]],
+        "R": [[3,"Q"]],
+        "U": [[4,120]],
+        "V": [[3,130],[2,90]]
     }
     
     #Determine frequency of skus
-    sku_freq = {
-        "A":0,
-        "B":0,
-        "C":0,
-        "D":0,
-        "E":0,
-        "F":0
-    }
+    sku_freq = dict.fromkeys(prices.keys(), 0)
     
     for sku in skus:
         if sku not in prices.keys():
@@ -60,8 +81,9 @@ def checkout(skus):
     
     return sum_price
             
-# print(checkout("ABCDEF"))  
+print(checkout("FFABCDECBAABCABBAAAEEAAFF"))  
 
     
         
+
 
