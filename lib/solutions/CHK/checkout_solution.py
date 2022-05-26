f294c478178cb1ca64b3710e5c7cbad98fc51a01
sbx_user1051 <sbx_user1051@169.254.33.21>
2022-05-26 20:06:01 +0000

    Thu May 26 20:06:01 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,43 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    #Price table definitions
+    prices = {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15
+    }
+    offers = {
+        "A": (3,130),
+        "B": (2,45)
+    }
+    
+    #Determine frequency of skus
+    sku_freq = {
+        "A":0,
+        "B":0,
+        "C":0,
+        "D":0
+    }
+    
+    for sku in skus:
+        if sku not in prices.keys():
+            return -1
+        sku_freq[sku] += 1
+    
+    #Price determination
+    sum_price = 0
+    
+    for item in sku_freq.keys():
+        if item in offers.keys():
+            number_offer = sku_freq[item]//offers[item][0]
+            sum_price += number_offer*offers[item][1] + (sku_freq[item]-number_offer)*prices[item]
+        else:
+            sum_price += sku_freq[item]*prices[item]
+            
+    
+
+    
+        
+
