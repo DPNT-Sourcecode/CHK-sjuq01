969ed5b009fe18663e2dca5fbc561397c3da7fd4
sbx_user1051 <sbx_user1051@169.254.33.21>
2022-05-26 20:22:53 +0000

    Thu May 26 20:22:53 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,11 +8,13 @@ def checkout(skus):
         "A": 50,
         "B": 30,
         "C": 20,
-        "D": 15
+        "D": 15,
+        "E": 40
     }
     offers = {
-        "A": (3,130),
-        "B": (2,45)
+        "A": [[5,200],[3,130]],
+        "B": [[2,45]],
+        "E": [[2,"B"]]
     }
     
     #Determine frequency of skus
@@ -20,7 +22,8 @@ def checkout(skus):
         "A":0,
         "B":0,
         "C":0,
-        "D":0
+        "D":0,
+        "E":0
     }
     
     for sku in skus:
@@ -30,11 +33,24 @@ def checkout(skus):
     
     #Price determination
     sum_price = 0
+    #Prioritisation of items
+    priority_order = ["E", "A", "B", "C", "D"]
     
-    for item in sku_freq.keys():
+    for item in priority_order:
         if item in offers.keys():
-            number_offer = sku_freq[item]//offers[item][0]
-            sum_price += number_offer*offers[item][1] + (sku_freq[item]-(number_offer*offers[item][0]))*prices[item]
+            for offer in offers[item]:
+                # Monetary offer
+                if isinstance(offer[1], int):
+                    number_offer = sku_freq[item]//offer[0]
+                    sku_freq[item] -= number_offer*offer[0] 
+                    sum_price += number_offer*offer[1]
+                
+                #Other item offer
+                elif isinstance(offer[1],str):
+                    number_offer = sku_freq[item]//offer[0]
+                    sku_freq[offer[1]] -= number_offer
+   
+            sum_price += sku_freq[item]*prices[item]
         else:
             sum_price += sku_freq[item]*prices[item]
     
@@ -44,3 +60,4 @@ def checkout(skus):
 
     
         
+
